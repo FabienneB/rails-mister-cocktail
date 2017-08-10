9e48dc4f1320d4e1e25dac36eabe0417e275cf9b
Fabienne Bremond <fabienne.bremond@gmail.com>
2017-08-10 15:08:05 +0200

    ajout vues

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,28 @@
+class DosesController < ApplicationController
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    if @dose.save
+      redirect_to cocktail_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktails_path
+  end
+
+
+   def dose_params
+    params.require(:cocktail).permit(:description)
+  end
+end
+
